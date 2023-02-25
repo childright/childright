156f98691f1895e9476f4acc68dd3eed9ccaa419
jhonadc <jhonathanaugusto@gmail.com>
2023-02-25 18:02:06 +0100

    created empty components files for wizardComment and ZuCommunity

---
+++ src/ui/wizardComment.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+type Props = {};
+
+class wizardComment extends PureComponent<Props> {
+  render() {
+    return <div>wizardComment</div>;
+  }
+}
+
+export default wizardComment;
+
+//temporary content. will be changed.
---
+++ src/ui/zuCommunity.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+type Props = {};
+
+class zuCommunity extends PureComponent<Props> {
+  render() {
+    return <div>zuCommunity</div>;
+  }
+}
+
+export default zuCommunity;
+
+//temporary content. will be changed.
