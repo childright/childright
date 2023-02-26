c72763394986d512e72907cfbec1328168e5e7bd
Mark Witt <mwitt@newstore.com>
2023-02-26 01:59:24 +0100

    Animation tryout persist rive

--- src/ui/StepperLayout.tsx
+++ src/ui/StepperLayout.tsx
@@ -2,6 +2,10 @@ import { Progress } from "@mantine/core";
 import { useRouter } from "next/router";
 import type { ReactNode } from "react";
 import React from "react";
+import {
+  AbsoluteAnimatedCharacter,
+  AnimatedCharacterElement,
+} from "./animations/AnimatedCharacter";
 
 type Props = {
   children: ReactNode;
@@ -23,6 +27,7 @@ const StepperLayout = ({ children }: Props) => {
   return (
     <div>
       <div className="mx-auto p-4 md:w-3/4 lg:w-1/2">{children}</div>
+      {AnimatedCharacterElement}
       <Progress
         className="fixed bottom-0 left-0 right-0"
         value={stepsToProgress[pathname as keyof typeof stepsToProgress]}
--- src/ui/animations/AnimatedCharacter.tsx
+++ src/ui/animations/AnimatedCharacter.tsx
@@ -1,5 +1,5 @@
-import type { CanvasHTMLAttributes, DetailedHTMLProps, FC } from "react";
-import { useRive } from "@rive-app/react-canvas";
+import { CanvasHTMLAttributes, DetailedHTMLProps, FC, useState } from "react";
+import Rive, { useRive } from "@rive-app/react-canvas";
 
 const AnimatedCharacter: FC<
   DetailedHTMLProps<CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>
@@ -7,6 +7,13 @@ const AnimatedCharacter: FC<
   const { rive, RiveComponent } = useRive({
     src: "/exampleCharacterAnimation.riv",
     autoplay: true,
+  });
+
+  const [counter, setCounter] = useState(0);
+
+  rive?.on("loop" as any, () => {
+    console.log(counter);
+    setCounter((c) => c + 1);
   });
 
   const togglePlaying = () => {
@@ -21,5 +28,7 @@ const AnimatedCharacter: FC<
 export default AnimatedCharacter;
 
 export const AbsoluteAnimatedCharacter: FC = () => (
-  <AnimatedCharacter className="absolute bottom-[-20px] left-0 h-56 w-56" />
+  <AnimatedCharacter className="absolute bottom-[-20px] left-[-35px] h-40 w-40 md:h-56 md:w-56" />
 );
+
+export const AnimatedCharacterElement = <AbsoluteAnimatedCharacter />;
