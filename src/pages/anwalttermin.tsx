6ae79d292e66443af76c2ef62f38bb7052104576
jhonadc <jhonathanaugusto@gmail.com>
2023-03-02 21:39:06 +0100

    anwalt termin page created

---
+++ src/pages/anwalttermin.tsx
@@ -0,0 +1,31 @@
+import { type NextPage } from "next";
+import { Formik, Form } from "formik";
+import StepperLayout from "../ui/StepperLayout";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
+import WizardComment from "../ui/WizardComment";
+import ZuCommunity from "../ui/ZurCommunity";
+import { Button } from "@mantine/core";
+
+
+const anwaltTermin: NextPage = () => {
+  return (
+    <StepperLayout>
+      <>
+        <h1 className="mb-4 text-center">Jugendamt - Anwalt Termin</h1>
+        <div className="grid grid-cols-2 gap-x-5">
+          <div>
+            <h4>Jugentamt / Anwalt setzt ein Schreiben auf an Unterhaltspflichtigen, um zu zahlen.</h4>
+          </div>
+        </div>
+        <div className="mt-20 grid grid-cols-2 gap-10">
+          {" "}
+          <ZuCommunity />
+          <WizardComment />
+        </div>
+      </>
+    </StepperLayout>
+  );
+};
+
+export default anwaltTermin;
