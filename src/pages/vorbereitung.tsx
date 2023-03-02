77f38c0f9a8b7486efa04c514f0e3be2f7b6bab8
jhonadc <jhonathanaugusto@gmail.com>
2023-03-02 21:57:34 +0100

    other pages created

---
+++ src/pages/vorbereitung.tsx
@@ -0,0 +1,32 @@
+import { type NextPage } from "next";
+import { Formik, Form } from "formik";
+import StepperLayout from "../ui/StepperLayout";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
+import WizardComment from "../ui/WizardComment";
+import ZuCommunity from "../ui/ZurCommunity";
+import { Button } from "@mantine/core";
+
+
+const vorbereitung: NextPage = () => {
+  return (
+    <StepperLayout>
+      <>
+        <h1 className="mb-4 text-center">Vorbereitung der Klage (Ratgeber, Infos)</h1>
+        <div className="grid grid-cols-2 gap-x-5">
+          <div>
+            <h4>Ratgeber Infos: Was passiert vor dem Familiengericht (Weiterführende Links)</h4>
+          </div>
+        </div>
+        <div className="mt-20 grid grid-cols-2 gap-10">
+          {" "}
+          <ZuCommunity />
+          <WizardComment />
+          {/* add prop to wizard component - no message on this page */}
+        </div>
+      </>
+    </StepperLayout>
+  );
+};
+
+export default vorbereitung;
