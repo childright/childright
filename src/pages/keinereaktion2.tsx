41a1bb089f066d79eb03b08618dbc5871363c6bb
jhonadc <jhonathanaugusto@gmail.com>
2023-03-02 21:30:05 +0100

    anwalt checkliste page complete

---
+++ src/pages/keinereaktion2.tsx
@@ -0,0 +1,68 @@
+import { type NextPage } from "next";
+import { Formik, Form } from "formik";
+import StepperLayout from "../ui/StepperLayout";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
+import WizardComment from "../ui/WizardComment";
+import ZuCommunity from "../ui/ZurCommunity";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+import {
+  Degree,
+  EducationSituation,
+  FamilyState,
+  Income,
+  LivingSituation,
+  ParentData,
+} from "@prisma/client";
+import type { ProfileStepData } from "@prisma/client";
+import { Select } from "formik-mantine";
+import NumberInputField from "../ui/NumberInputField";
+import DatePickerField from "../ui/DatePickerField";
+import { Button } from "@mantine/core";
+import { api } from "../utils/api";
+
+const exampleText = "Loaded from constant";
+
+const keinereaktion2: NextPage = () => {
+  return (
+    <StepperLayout>
+      <>
+        <h1 className="mb-4 text-center">Jugendamt - Anwalt Checkliste</h1>
+
+ 
+        <div className="grid grid-cols-2 gap-x-5">
+                  
+          <div>
+            <h3>Folgende Dokumente werden für den Termin benötigt:</h3>
+            <ul>
+              <li>Geburtsurkunde des Kindes</li>
+              <li>Titel vom Gericht (vergangene Prozesse?)Nachweis über Unterhaltszahlungen</li>
+              <li>Scheidungsurteil bzw. Schreiben des Anwalts/ der Anwältin, sofern vorhanden</li>
+              <li>Gehaltsnachweise der letzten 12 Monate</li>
+              <li>Unterhaltstitel im Original in der 1. vollstreckbaren Ausfertigung (Beschluss, Urkunde, Vergleich)</li>
+              <li>Kopie des Personalausweises/ Passes des Antragstellers</li>
+              <li>bei Ausländern: Aufenthaltstitel in Form einer Niederlassungserlaubnis bzw. Aufenthaltserlaubnis</li>
+              <li>Aufenthaltsbescheinigungen</li>
+              <li>ggbfls. Vaterschaftsanerkennung</li>
+            </ul>
+          </div>
+
+          <div className="m-auto text-center">
+            <h4 >
+              Vorlagen, um fehlende Dokumente zu beantragen.
+            </h4><Button>Weiter</Button>
+          </div>
+          
+
+        </div>
+        <div className="mt-20 grid grid-cols-2 gap-10">
+          {" "}
+          <ZuCommunity />
+          <WizardComment />
+        </div>
+      </>
+    </StepperLayout>
+  );
+};
+
+export default keinereaktion2;
