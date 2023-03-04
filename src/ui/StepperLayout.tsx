3f3e14dafcb7a081a7697a5aa37be45e00ea1888
Mark Witt <mwitt@newstore.com>
2023-03-04 10:56:06 +0100

    Added Progress Bar to StepperLayout

--- src/ui/StepperLayout.tsx
+++ src/ui/StepperLayout.tsx
@@ -1,3 +1,5 @@
+import { Progress } from "@mantine/core";
+import { useRouter } from "next/router";
 import type { ReactNode } from "react";
 import React from "react";
 
@@ -5,11 +7,29 @@ type Props = {
   children: ReactNode;
 };
 
+const stepsToProgress = {
+  "/calculate": 5,
+  "/profile": 20,
+  "/mother": 30,
+  "/father": 40,
+  "/sibling": 50,
+  "/resultAmount": 60,
+  "/resultTemplates": 70,
+};
+
 const StepperLayout = ({ children }: Props) => {
+  const { pathname } = useRouter();
+
   return (
-    <div className="mx-auto md:w-3/4 lg:w-1/2">
-      {children}
-      {/*  <AbsoluteAnimatedCharacter /> */}
+    <div className="flex h-full flex-col">
+      <div className="mx-auto md:w-3/4 lg:w-1/2">
+        {children}
+        {/*  <AbsoluteAnimatedCharacter /> */}
+      </div>
+      <Progress
+        className="mt-auto mb-2"
+        value={stepsToProgress[pathname as keyof typeof stepsToProgress]}
+      />
     </div>
   );
 };
