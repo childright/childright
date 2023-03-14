5d5a7caefdefaf077af43fca3f6237f394c0e432
Mark Witt <mwitt@newstore.com>
2023-03-14 18:12:11 +0100

    added precommit hook and fixed eslint syntax error

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,38 +7,44 @@ import WizardComment from "../ui/WizardComment";
 import ZuCommunity from "../ui/ZurCommunity";
 import { Button } from "@mantine/core";
 
-
 const dashboard: NextPage = () => {
   return (
     <StepperLayout>
       <>
         <h1 className="mb-4 text-center">Dashboard</h1>
 
- 
         <div className="grid grid-cols-2 gap-x-5">
-                  
           <div>
             <h3>Must include:</h3>
             <ul>
-              
-              <li>Profile (eigenes Profil, Mutter, Vater, Geschwisterkinder..)</li>
-              <li>“Progress Bar” -> zeigt in welchem Abschnitt man sich befindet (90%)</li>
-              <li>zeigen, welche Dokumente noch nachgereicht werden müssen für nächsten Schritten</li>
+              <li>
+                Profile (eigenes Profil, Mutter, Vater, Geschwisterkinder..)
+              </li>
+              <li>
+                “Progress Bar” -{">"} zeigt in welchem Abschnitt man sich
+                befindet (90%)
+              </li>
+              <li>
+                zeigen, welche Dokumente noch nachgereicht werden müssen für
+                nächsten Schritten
+              </li>
               <li>Community</li>
               <li>Ratgeber/FAQ </li>
               <li>Kontakt (zu uns, aber auch externe Beratung etc.)</li>
-              <li>bei Ausländern: Aufenthaltstitel in Form einer Niederlassungserlaubnis bzw. Aufenthaltserlaubnis</li>
+              <li>
+                bei Ausländern: Aufenthaltstitel in Form einer
+                Niederlassungserlaubnis bzw. Aufenthaltserlaubnis
+              </li>
               <li>Aufenthaltsbescheinigungen</li>
               <li>ggbfls. Vaterschaftsanerkennung</li>
             </ul>
           </div>
 
           <div className="m-auto text-center">
-            <h4 > </h4>
+            <h4> </h4>
           </div>
         </div>
         <div className="mt-20 grid grid-cols-2 gap-10">
-          {" "}
           <WizardComment />
         </div>
       </>
