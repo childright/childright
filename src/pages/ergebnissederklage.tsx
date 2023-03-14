f21c9389cdc867dce772bb083e68b81c7eacc8bd
Mark Witt <mwitt@newstore.com>
2023-03-14 18:12:11 +0100

    added precommit hook and fixed eslint syntax error

--- src/pages/ergebnissederklage.tsx
+++ src/pages/ergebnissederklage.tsx
@@ -7,19 +7,22 @@ import WizardComment from "../ui/WizardComment";
 import ZuCommunity from "../ui/ZurCommunity";
 import { Button } from "@mantine/core";
 
-
 const ergebnisseDerKlage: NextPage = () => {
   return (
     <StepperLayout>
       <>
-        <h1 className="mb-4 text-center">Ergebnisse der Klage (Ratgeber, Infos)</h1>
+        <h1 className="mb-4 text-center">
+          Ergebnisse der Klage (Ratgeber, Infos)
+        </h1>
         <div className="grid grid-cols-2 gap-x-5">
           <div>
-            <h4>Dokumente hochladen -> Ergebnis (Der Titel des Gerichts kann in eigene Library hochgeladen werden)</h4>
+            <h4>
+              Dokumente hochladen -{">"} Ergebnis (Der Titel des Gerichts kann
+              in eigene Library hochgeladen werden)
+            </h4>
           </div>
         </div>
         <div className="mt-20 grid grid-cols-2 gap-10">
-          {" "}
           <ZuCommunity />
           <WizardComment />
           {/* add prop to wizard component - no message on this page */}
