47f0dc0658770dfb2fa0b75958796d0e1ca0aea8
Mark Witt <mwitt@newstore.com>
2023-03-23 20:25:35 +0100

    basic email endpoint using nodemailer

--- src/pages/api/emailCron.ts
+++ src/pages/api/emailCron.ts
@@ -1,5 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "John Doe" });
+import nodemailer from "nodemailer";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const data = await prisma.calculatorStepEmail.findMany();
+  console.log(data);
+
+  // send a a mail with nodemailer
+  // https://nodemailer.com/about/
+
+  const transporter = await nodemailer.createTransport({
+    host: "mail.privateemail.com",
+    port: 587,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const result = await transporter.sendMail({
+    from: process.env.SMTP_EMAIL,
+    to: "markwitt6@yahoo.de",
+    subject: "Hello",
+    text: "Hello world",
+  });
+
+  res.send({ result });
 }
