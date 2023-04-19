12cdcb3fa2b6ac8f9daa9457f999c3a222d4057d
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2023-04-19 12:36:58 +0200

    Add end to end use case

--- tests/legalProcess.spec.ts
+++ tests/legalProcess.spec.ts
@@ -7,17 +7,17 @@ test("Application flow testing legalProcess to legalActions", async ({
   await page.getByRole("heading", {
     name: "Rechtsweg - Unterhalt einklagen",
   });
-  await page.getByRole("heading", {
-    name: "Option 1: Jugentamt stellt Anwalt",
-  });
   await page.waitForSelector("a[data-button='true'][href='/legalActions']", {
     timeout: 3000,
   });
+  console.log("Clicking button...");
   await page.getByRole("link", { name: "Weiter" }).click();
+  console.log("Button clocked YA MANYAK!");
+  await page.waitForSelector("h4");
   await page.waitForFunction(() => {
     const h4 = document.querySelector("h4");
     return h4 && h4.innerText === "Option 1: Jugentamt stellt Anwalt";
   });
-
-  await expect(page).toHaveURL(/legalActions/);
+  await page.waitForURL("http://localhost:3000/legalActions");
+  await expect(page).toHaveURL("http://localhost:3000/legalActions");
 });
