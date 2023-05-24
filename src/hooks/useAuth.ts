3b7cc48e8e16a83ea7ce2d4aad563a40a9471e58
Ignazio Balistreri <ignazio.balistreri@code.berlin>
2023-05-24 13:16:29 +0200

    Add error message

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
+import { showNotification } from "@mantine/notifications";
 
 export default function useRedirectUnauthenticated() {
   const data = useSession();
@@ -8,6 +9,11 @@ export default function useRedirectUnauthenticated() {
 
   useEffect(() => {
     if (data.status === "unauthenticated") {
+      showNotification({
+        title: "User Unauthenticated",
+        message: `Redirecting...`,
+        color: "red",
+      });
       router.push("/");
     }
   }, [router, data.status]);
